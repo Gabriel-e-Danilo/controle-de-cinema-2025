6c5cc6a3a68063c061ae76a5da8386b4169a5942
danilo <danilocencigg@gmail.com>
2025-08-26 16:15:49 -0300

    fix duplicate projects

---
+++ ControleDeCinema.Testes.Unidade/ModuloSessao/SessaoTests.cs
@@ -0,0 +1,59 @@
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Dominio.ModuloSessao;
+
+namespace ControleDeCinema.Testes.Unidade_1_;
+
+[TestClass]
+public class SessaoTests
+{
+    private Filme? filme;
+    
+    private Sala? sala;
+
+    private Sessao? sessao;
+
+    private Ingresso? ingresso;
+
+    [TestMethod]
+    public void Deve_Gerar_Ingresso_Corretamente()
+    {
+        sessao = new Sessao(DateTime.Now.AddHours(1), 50, filme!, sala!);
+
+        ingresso = sessao.GerarIngresso(10, true);
+
+        Ingresso ingressoEsperado = new Ingresso(10, true, sessao);
+
+        Assert.AreEqual(ingressoEsperado, ingresso);
+    }
+
+    [TestMethod]
+    public void Deve_Obter_Assentos_Disponiveis_Corretamente()
+    {
+        sessao = new Sessao(DateTime.Now.AddHours(1), 5, filme!, sala!);
+        
+        sessao.GerarIngresso(1, false);
+        sessao.GerarIngresso(2, true);
+        sessao.GerarIngresso(3, false);
+
+        int[] assentosDisponiveis = sessao.ObterAssentosDisponiveis();
+        int[] assentosEsperados = new int[] { 4, 5 };
+
+        CollectionAssert.AreEqual(assentosEsperados, assentosDisponiveis);
+    }
+
+    [TestMethod]
+    public void Deve_Obter_Quantidade_Ingressos_Disponiveis_Corretamente()
+    {
+        sessao = new Sessao(DateTime.Now.AddHours(1), 5, filme!, sala!);
+
+        sessao.GerarIngresso(1, false);
+        sessao.GerarIngresso(2, true);
+        sessao.GerarIngresso(3, false);
+
+        int quantidadeIngressosDisponiveis = sessao.ObterQuantidadeIngressosDisponiveis();
+        int quantidadeEsperada = 2;
+
+        Assert.AreEqual(quantidadeEsperada, quantidadeIngressosDisponiveis);
+    }
+}
