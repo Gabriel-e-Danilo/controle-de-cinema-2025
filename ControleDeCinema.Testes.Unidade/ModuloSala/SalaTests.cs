a63205cfd6ac9fa5213bdfa60c36cddb80c00fab
gkirschnxr <kirschnxr@gmail.com>
2025-08-30 12:02:48 -0300

    Atribuido Testes de Unidade para SalaTests

---
+++ ControleDeCinema.Testes.Unidade/ModuloSala/SalaTests.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeCinema.Dominio.ModuloSala;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloSala;
+
+[TestClass]
+[TestCategory("Testes de Unidade de Sala")]
+public class SalaTests
+{
+    [TestMethod]
+    public void AtualizarRegistro_DeveAtualizarDescricao() {
+
+        // Arrange
+        var salaOriginal = new Sala(1, 100);
+        var salaEditada = new Sala(2, 150);
+
+        // Act
+        salaOriginal.AtualizarRegistro(salaEditada);
+
+        // Assert
+        Assert.AreEqual(2, salaOriginal.Numero);
+        Assert.AreEqual(150, salaOriginal.Capacidade);
+    }
+}
